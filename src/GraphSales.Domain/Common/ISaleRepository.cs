d49596449329af10e4662726e473f6da1d038b8f
Dmitriy Tereschenko <dmitriy.tereshchenko1989@gmail.com>
2024-07-01 20:37:31 +0300

    implemented layers

---
+++ src/GraphSales.Domain/Common/ISaleRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphSales.Domain.Common
+{
+    using GraphSales.Domain.Entities;
+    using GraphSales.Domain.Enums;
+
+    public interface ISaleRepository
+    {
+        Task SaveSalesAsync(List<SaleModel> sales);
+        IQueryable<SaleModel> GetSalesByPeriod(SaleInterval period, DateTimeOffset startPeriod, DateTimeOffset endPeriod);
+    }
+}
---
+++ src/GraphSales.Domain/Common/ISaleService.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphSales.Domain.Common
+{
+    using GraphSales.Domain.Entities;
+    using GraphSales.Domain.Enums;
+
+    public interface ISaleService
+    {
+        IQueryable<SaleModel> GetSalesByPeriod(SaleInterval period, DateTimeOffset startPeriod, DateTimeOffset endPeriod);
+        Task SaveSalesAsync(List<SaleModel> sales);
+    }
+}
